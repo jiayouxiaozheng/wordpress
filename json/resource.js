d1a2649e302bccbc9a796b67852938e31b31c233
jiayouxiaozheng <Simin168a>
2018-05-29 13:57:46 +0800

    添加漫画 修改article

--- json/resource.js
+++ json/resource.js
@@ -48,16 +48,38 @@
           articleListNum:451,
           webUrl:"http://www.manhuatai.com/douluodalu/"
         },
+        {
+          name:"偷星九月天漫画",
+          type:"touxing",
+          source:"yantai",
+          imgUrl:[
+            "http://mhpic.mh51.com/comic/T%2F%E5%81%B7%E6%98%9F%E4%B9%9D%E6%9C%88%E5%A4%A9%2F"
+          ],
+          articleNum:447,
+          articleListNum:447,
+          webUrl:"http://www.manhuatai.com/touxingjiuyuetian/"
+        },
+        {
+          name:"霸道忠犬寻爱记漫画",
+          type:"badaozongquan",
+          source:"yantai",
+          imgUrl:[
+            "http://mhpic.mh51.com/comic/B%2F%E9%9C%B8%E9%81%93%E5%BF%A0%E7%8A%AC%E5%AF%BB%E7%88%B1%E8%AE%B0%2F"
+          ],
+          articleNum:456,
+          articleListNum:456,
+          webUrl:"http://www.manhuatai.com/bdzqxaj/"
+        },
 		{
-    	  name:"偷星九月天漫画",
-    	  type:"touxing",
+    	  name:"纯情花嫁漫画",
+    	  type:"cunqinghuajia",
     	  source:"yantai",
     	  imgUrl:[
-    	    "http://mhpic.mh51.com/comic/T%2F%E5%81%B7%E6%98%9F%E4%B9%9D%E6%9C%88%E5%A4%A9%2F"
+    	    "http://mhpic.mh51.com/comic/C%2F%E7%BA%AF%E6%83%85%E4%B8%AB%E5%A4%B4%E7%81%AB%E8%BE%A3%E8%BE%A3%2F"
     	  ],
-    	  articleNum:447,
-    	  articleListNum:447,
-    	  webUrl:"http://www.manhuatai.com/touxingjiuyuetian/"
+    	  articleNum:460,
+    	  articleListNum:460,
+    	  webUrl:"http://www.manhuatai.com/chunqingyatouhuolala/"
 		},
 		{
     	  name:"一拳超人漫画",
@@ -80,15 +102,25 @@
     	  articleListNum:386,
     	  webUrl:"http://ssg.feiwan.net/xiangguan/"
 		},{
-    	  name:"全职猎人漫画",
-    	  type:"lieren",
+          name:"全职猎人漫画",
+          type:"lieren",
+          source:"feiwan",
+          imgUrl:[
+            "http://img.feiwan.net/hunter/manhua/"
+          ],
+          articleNum:395,
+          articleListNum:395,
+          webUrl:"http://hunter.feiwan.net/manhua/"
+        },{
+    	  name:"进击的巨人漫画",
+    	  type:"juren",
     	  source:"feiwan",
     	  imgUrl:[
-            "http://img.feiwan.net/hunter/manhua/"
+            "http://img.feiwan.net/juren/manhua/"
     	  ],
-    	  articleNum:395,
-    	  articleListNum:395,
-    	  webUrl:"http://hunter.feiwan.net/manhua/"
+    	  articleNum:464,
+    	  articleListNum:464,
+    	  webUrl:"http://juren.feiwan.net/manhua/"
 		}
 	];
     
